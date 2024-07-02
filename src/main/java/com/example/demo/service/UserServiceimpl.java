92d536efc67f2c2abe323ba8cab18b14f48ea613
dixittagadiya <139606709+dixittagadiya@users.noreply.github.com>
2024-07-02 11:21:59 +0530

    Update UserServiceimpl.java

--- src/main/java/com/example/demo/service/UserServiceimpl.java
+++ src/main/java/com/example/demo/service/UserServiceimpl.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.User;
 import com.example.demo.repo.UserRepo;
 
+@Service
 public class UserServiceimpl implements UserService	 {
 
 	@Autowired
